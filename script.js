6c42c517ba1ec360305328550cc6232ff798b9b8
Danyllo Danesi <danyllo.danesi@gmail.com>
2023-04-20 22:34:59 -0300

    calculadora quase completa

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function clean(){
 
     document.querySelector(".display").innerHTML = "";
 }
+function back ()
+{
+    let display = document.querySelector(".display").innerHTML;
+    document.querySelector(".display").innerHTML =display.substring(0,display.length -1);
+}
+function sum(){
+    let sum = document.querySelector(".display").innerHTML;
+    if(sum){
+         document.querySelector(".display").innerHTML= eval(sum);
+    }
+    else{
+        document.querySelecto(".display").innerHTML= "nenhum comando";
+    }
+}
